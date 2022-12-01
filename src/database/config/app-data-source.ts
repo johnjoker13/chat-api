925756f9412d5d58280fce467a9948ed3a766e96
johnjoker13 <joaoavelino2008@hotmail.com>
2022-12-01 17:21:41 -0300

    refactor(app-data-source.ts): removes properties for production mode

--- src/database/config/app-data-source.ts
+++ src/database/config/app-data-source.ts
@@ -1,15 +1,14 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-const appDataSource: DataSourceOptions = {
+export const appDataSource: DataSourceOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
   username: 'postgres',
   password: 'postgres',
   database: 'chat_db',
-  entities: ['src/entities/*entity.ts'],
-  logging: true,
-  synchronize: true,
+  entities: ['src/**/*entity.{ts,js}'],
+  migrations: ['src/migrations/**/*.{ts,js}'],
 };
 
 export const config = new DataSource(appDataSource);
